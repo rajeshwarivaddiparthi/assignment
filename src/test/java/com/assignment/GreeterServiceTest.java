a2004bb7ab74d1db56fb4ed6afceaa31fe0ae384
rvaddiparthi <rajeshwari.vaddiparthi@gmail.com>
2023-10-29 16:51:35 +0000

    Create GreeterServiceTest.java
    unit test to test capitalising name

---
+++ src/test/java/com/assignment/GreeterServiceTest.java
@@ -0,0 +1,34 @@
+package com.assignment;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+public class GreeterServiceTest {
+
+    @InjectMocks
+    private GreeterService greeterService;
+
+    @BeforeEach
+    public void setUp() {
+        Clock clock = Clock.fixed(Instant.from(ZonedDateTime.parse("2023-10-29T10:00:00Z")), ZoneId.systemDefault());
+        greeterService = new GreeterService(clock);
+    }
+
+    @Test
+    public void shouldCapitalizeName() {
+        String name = greeterService.getGreetMessage("john");
+        assertThat(name, is("Good morning john"));
+    }
+}
